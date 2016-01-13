1bde67931008832542ba98b734e56135169b7c2e
bkazic <blaz.kazic@ijs.si>
2016-01-13 09:01:58 +0000

    Default mode for service start is 'open'. Commented out creating backup immediately after server starts. Added 'deleteFolderRecursive' function in utils/helper.js. Added parameter '--max-old-space-size=8192' to "npm start". In predictionService.js, 'openMode' was changed to 'openFromDb' In predictionService.js, 'open' mode was added (first it tries to open in 'openFromDb' mode, if not sucessfull, it tries to open in 'restoreFromBackup' mode)

--- TrafficPrediction/index.js
+++ TrafficPrediction/index.js
@@ -10,7 +10,7 @@ var server = require('./server/server.js');
 var trafficPrediction = new TrafficPrediction();
 
 // read input script argument for mode type. Default is "cleanCreate"
-var mode = (process.argv[2] == null) ? "cleanCreateLoad" : process.argv[2];
+var mode = (process.argv[2] == null) ? "open" : process.argv[2];
 predictionService.start(trafficPrediction, mode);
 
 if (env === 'production') {
@@ -30,6 +30,6 @@ if (env === 'production') {
 server.init(trafficPrediction);
 server.start(config.trafficPredictionService.server.port);
 
-// create backup before running server (if in production mode)
-if (env === 'production') trafficPrediction.backup(true);
+// create backup before running server
+//trafficPrediction.backup(true);
 //trafficPrediction.backupAsync(true);
